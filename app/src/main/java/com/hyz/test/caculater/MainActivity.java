94ac0656aac45fd6f49674a1ab4b2b3eb2576f50
hyz1992 <18086089679@163.com>
2016-12-02 00:38:22 +0800

    添加科学计算器布局以及动画，修改部分计算逻辑bug

--- app/src/main/java/com/hyz/test/caculater/MainActivity.java
+++ app/src/main/java/com/hyz/test/caculater/MainActivity.java
@@ -2,6 +2,7 @@ package com.hyz.test.caculater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -11,6 +12,9 @@ import android.view.animation.AnimationSet;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +28,9 @@ public class MainActivity extends AppCompatActivity{
     TextView text_express;
     boolean m_isNoInput = true;//是不是还没有开始输入（初始化时、点了“=”时）
     Manger m_manger=new Manger();
+    boolean _isNormalType = true;
+    boolean m_isTurning = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,10 +43,19 @@ public class MainActivity extends AppCompatActivity{
         text_result.setText("");
 
         m_express.add("0");
+
+        _isNormalType = true;
+
+        DisplayMetrics dm2 = getResources().getDisplayMetrics();
+        View result_panel = findViewById(R.id.result_panel);
+        RelativeLayout.LayoutParams linearParams = (RelativeLayout.LayoutParams) result_panel.getLayoutParams();
+        linearParams.height = dm2.heightPixels*3/8;
+        result_panel.setLayoutParams(linearParams);
+
     }
-    private Animation getScaleAni(float form,float to,long duration){
-        Animation scaleAnimation=new ScaleAnimation(form,to,form,to,
-                Animation.RELATIVE_TO_SELF,1.0f,Animation.RELATIVE_TO_SELF,0.5f);
+    private Animation getScaleAni(float formX,float toX,float formY,float toY,long duration,float archX,float archY){
+        Animation scaleAnimation=new ScaleAnimation(formX,toX,formY,toY,
+                Animation.RELATIVE_TO_SELF,archX,Animation.RELATIVE_TO_SELF,archY);
 
         scaleAnimation.setDuration(duration);
         scaleAnimation.setInterpolator(new DecelerateInterpolator());
@@ -75,33 +91,122 @@ public class MainActivity extends AppCompatActivity{
 
         AnimationSet ani_2=new AnimationSet(true);
         ani_2.addAnimation(getTranslateAniY(fromY - toY,0.0f,200));
-        ani_2.addAnimation(getScaleAni(1.0f,0.6f,200));
+        ani_2.addAnimation(getScaleAni(1.0f,0.6f,1.0f,0.6f,200,1.0f,0.5f));
         ani_2.setFillAfter(true);
         text_result.startAnimation(ani_2);
     }
+
+    private void runTurnAction(){
+        if(m_isTurning)
+            return;
+        _isNormalType = !_isNormalType;
+        m_isTurning = true;
+        final View frist_panel = findViewById((R.id.frist_panel));
+        final View secend_panel = findViewById((R.id.secend_panel));
+        secend_panel.setVisibility(View.VISIBLE);
+        Animation scaleAnimation;
+        final long duration = 500;
+        if(_isNormalType)
+            scaleAnimation = getScaleAni(1.0f,1.25f,1.0f,1.2f,duration,1.0f,1.0f);//从科学型放大到普通型
+        else
+            scaleAnimation = getScaleAni(1.25f,1.0f,1.2f,1.0f,duration,1.0f,1.0f);
+        scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                Log.e(TAG, "---start!");
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Log.e(TAG, "---end!");
+                m_isTurning = false;
+                secend_panel.clearAnimation();
+                secend_panel.invalidate();
+                if(_isNormalType){
+                    secend_panel.setVisibility(View.GONE);
+                    frist_panel.setVisibility(View.VISIBLE);
+                }else{
+                    secend_panel.setVisibility(View.VISIBLE);
+                    frist_panel.setVisibility(View.INVISIBLE);
+                }
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                Log.e(TAG, "---repeat!");
+            }
+        });
+
+        secend_panel.startAnimation(scaleAnimation);
+    }
     public void onClick(View v)
     {
-        int id = v.getId();
+        Button btn = (Button)v;
+        int id = btn.getId();
         String str = "";
         switch(id)
         {
-            case R.id.btn_0:str="0";break;
-            case R.id.btn_1:str="1";break;
-            case R.id.btn_2:str="2";break;
-            case R.id.btn_3:str="3";break;
-            case R.id.btn_4:str="4";break;
-            case R.id.btn_5:str="5";break;
-            case R.id.btn_6:str="6";break;
-            case R.id.btn_7:str="7";break;
-            case R.id.btn_8:str="8";break;
-            case R.id.btn_9:str="9";break;
-            case R.id.btn_point:str=".";break;
-            case R.id.btn_add:str="+";break;
-            case R.id.btn_sub:str="-";break;
-            case R.id.btn_multiply:str="×";break;
-            case R.id.btn_devide:str="÷";break;
-            case R.id.btn_equal:str="=";break;
+            case R.id.btn_0:
+            case R.id.btn_0_2:
+            case R.id.btn_1:
+            case R.id.btn_1_2:
+            case R.id.btn_2:
+            case R.id.btn_2_2:
+            case R.id.btn_3:
+            case R.id.btn_3_2:
+            case R.id.btn_4:
+            case R.id.btn_4_2:
+            case R.id.btn_5:
+            case R.id.btn_5_2:
+            case R.id.btn_6:
+            case R.id.btn_6_2:
+            case R.id.btn_7:
+            case R.id.btn_7_2:
+            case R.id.btn_8:
+            case R.id.btn_8_2:
+            case R.id.btn_9:
+            case R.id.btn_9_2:
+            case R.id.btn_point:        // .
+            case R.id.btn_point_2:      // .
+            case R.id.btn_add:          //＋
+            case R.id.btn_add_2:        //＋
+            case R.id.btn_sub:          //－
+            case R.id.btn_sub_2:        //－
+            case R.id.btn_multiply:     //×
+            case R.id.btn_multiply_2:   //×
+            case R.id.btn_devide:       //÷
+            case R.id.btn_devide_2:     //÷
+            case R.id.btn_sin:
+            case R.id.btn_cos:
+            case R.id.btn_tan:
+            case R.id.btn_In:
+            case R.id.btn_log:
+            case R.id.btn_bracket_left://(
+            case R.id.btn_bracket_right://)
+            case R.id.btn_factorial://!
+            case R.id.btn_power://开方
+            case R.id.btn_sqrt://开根号
+                str = btn.getText().toString();
+                str +="";
+                break;
+            case R.id.btn_e://自然底数
+                break;
+            case R.id.btn_pi://圆周率，π
+                break;
+            case R.id.btn_equal:
+            case R.id.btn_equal_2:
+                m_isNoInput = true;
+                if(m_express.size()<=1)
+                    return;
+                String resultStr = m_manger.caculate(m_express);
+                String expressStr = text_express.getText().toString();
+                text_result.setText(expressStr);
+                m_express.clear();
+                m_express.add(resultStr);
+                text_express.setText(resultStr);
+                m_tmpStr = resultStr;
+                runResultAction();
+                break;
             case R.id.btn_clear:
+            case R.id.btn_clear_2:
                 m_express.clear();
                 m_tmpStr="0";
                 m_express.add(m_tmpStr);
@@ -109,6 +214,7 @@ public class MainActivity extends AppCompatActivity{
                 text_result.setText("");
                 break;
             case R.id.btn_delete:
+            case R.id.btn_delete_2:
                 if(m_express.size()==1){
                     m_express.clear();
                     m_tmpStr="0";
@@ -134,25 +240,13 @@ public class MainActivity extends AppCompatActivity{
                 text_express.setText(_text);
                 break;
             case R.id.btn_turn:
-                Toast.makeText(MainActivity.this, R.string.notOpen, Toast.LENGTH_SHORT).show();
+                runTurnAction();
                 break;
             default:
                 break;
         }
 
-        if(str.equals("=")){
-            m_isNoInput = true;
-            if(m_express.size()<=1)
-                return;
-            String resultStr = m_manger.caculate(m_express);
-            String expressStr = text_express.getText().toString();
-            text_result.setText(expressStr);
-            m_express.clear();
-            m_express.add(resultStr);
-            text_express.setText(resultStr);
-            m_tmpStr = resultStr;
-            runResultAction();
-        }else if(!str.equals("")){
+        if(!str.equals("")){
             if(str.equals(".")){
                 if(Manger.isNum(m_tmpStr)&&m_tmpStr.indexOf(".")==-1){
                     m_tmpStr+=str;
@@ -169,15 +263,28 @@ public class MainActivity extends AppCompatActivity{
                         }
 
                         m_express.set(m_express.size()-1,m_tmpStr);
-                    }else{//这次是操作符
+                    }else if(!m_manger.canBehindNumber(str)){//这次是操作符，但不合法，如“9(”、“9sin”
+                        Toast.makeText(this, R.string.donotAllow, Toast.LENGTH_SHORT).show();
+                    }
+                    else{//这次是操作符，但合法
                         m_express.add(str);
                         m_tmpStr = str;
                     }
                 }else{//上次输入的是操作符
-                    if(Manger.isNum(str)) {//这次也是数字
+                    if(Manger.isNum(str)) {//这次是数字
+                        if(!m_manger.canFrontNumber(m_tmpStr)){//但是不合法，如")9"、"!9"
+                            Toast.makeText(this, R.string.donotAllow, Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            m_express.add(str);
+                            m_tmpStr = str;
+                        }
+
+                    }else if(m_manger.allowTwoOperator(m_tmpStr,str)){//两次输入字符，合法
                         m_express.add(str);
                         m_tmpStr = str;
-                    }else{
+                    }
+                    else{//两次输入字符，但不合法
                         m_express.set(m_express.size()-1,str);
                         m_tmpStr = str;
                     }
--- app/src/main/java/com/hyz/test/caculater/Manger.java
+++ app/src/main/java/com/hyz/test/caculater/Manger.java
@@ -11,21 +11,74 @@ import java.util.Stack;
 public class Manger {
     private Stack<Double> operandStack=new Stack<Double>();//操作数堆栈
 
-    private Map<String,Integer> priorityMap=new HashMap<String,Integer>();//用于存储操作符优先级的Map
+    static private Map<String,Integer> priorityMap=new HashMap<String,Integer>();//用于存储操作符优先级的Map
+    static private Map<String,String> allowMap=new HashMap<String,String>();//记录允许连续存在的操作符
     //初始化优先级约定(可根据计算的复杂程度扩展)
     public Manger()
     {
-        priorityMap.put("+",0);
-        priorityMap.put("-",0);
-        priorityMap.put("×", 1);
-        priorityMap.put("÷", 1);
+        priorityMap.put("+",0);//"＋"
+        priorityMap.put("-",0);//－
+        priorityMap.put("×", 1);//×
+        priorityMap.put("÷", 1);//÷
+        priorityMap.put("sin", 2);//sin
+        priorityMap.put("cos", 2);//cos
+        priorityMap.put("tan", 2);//tan
+        priorityMap.put("log", 3);//log
+        priorityMap.put("ln", 3);//ln
+        priorityMap.put("√", 4);//√
+        priorityMap.put("^", 4);//^
+        priorityMap.put("!", 4);//!
+        priorityMap.put("(", -1);//!
+        priorityMap.put(")", 5);//!
+
+        allowMap.put("+","(");
+        allowMap.put("-","(");
+        allowMap.put("×","(");
+        allowMap.put("÷","(");
+        allowMap.put("sin","(");
+        allowMap.put("cos","(");
+        allowMap.put("tan","(");
+        allowMap.put("log","(");
+        allowMap.put("ln","(");
+        allowMap.put("√","(");
+
+        allowMap.put(")","!");
+        allowMap.put(")","^");
     }
 
     private static boolean IsOperator(String str){
-        return !isNum(str);
+        boolean ret = priorityMap.containsKey(str);
+        return ret;
     }
     public static boolean isNum(String str){
-        return str.matches("-?[0-9]+.*[0-9]*");
+        return !IsOperator(str);
+    }
+
+    public static boolean allowTwoOperator(String frist,String secend){
+        return allowMap.get(frist).equals(secend);
+    }
+
+    //判断操作符能否出现在数字前
+    public static boolean canFrontNumber(String str){
+        //只能出现在数字后的操作符
+        ArrayList<String> cannotList = new ArrayList<String>();
+        cannotList.add(")");
+        cannotList.add("^");
+        cannotList.add("!");
+        return !cannotList.contains(str);
+    }
+
+    public static boolean canBehindNumber(String str){
+        //只能出现在数字前的操作符
+        ArrayList<String> cannotList = new ArrayList<String>();
+        cannotList.add("sin");
+        cannotList.add("cos");
+        cannotList.add("tan");
+        cannotList.add("log");
+        cannotList.add("ln");
+        cannotList.add("√");
+        cannotList.add("(");
+        return !cannotList.contains(str);
     }
 
     private int getPriority(String op)//得到一个操作符的优先级
@@ -42,16 +95,19 @@ public class Manger {
             if(str.equals("(")){
                 _stack.push(str);
             }else if(str.equals(")")){
-                String tmpStr;
-                while(!(tmpStr=_stack.pop()).equals("(")){
+                String tmpStr = _stack.lastElement();
+                while(!tmpStr.equals("(")){
                     ret.add(tmpStr);
+                    _stack.pop();
+                    tmpStr = _stack.lastElement();
                 }
+                _stack.pop();
             }else{
                 if(!IsOperator(str)){
                     ret.add(str);
                 }else{
-                    while(_stack.size()!=0&&getPriority(_stack.firstElement())>=getPriority(str)){
-                        ret.add(_stack.firstElement());
+                    while(_stack.size()!=0&&getPriority(_stack.lastElement())>=getPriority(str)){
+                        ret.add(_stack.lastElement());
                         _stack.pop();
                     }
                     _stack.push(str);
@@ -86,7 +142,7 @@ public class Manger {
                 _stack.push(value.toString());
             }
         }
-        String ret = _stack.firstElement();
+        String ret = _stack.lastElement();
         if(ret.contains(".0")){
             ret = ret.substring(0,ret.length()-2);
         }
